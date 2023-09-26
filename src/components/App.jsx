9024342b1316326a17d7e26d87a613d0ab2a6ef9
laxmanroy <luxmanroyluxman@gmail.com>
2023-09-27 00:14:52 +0600

    add all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,34 @@
-/*
-* Title: This is main component
-* Description: All work is in component
-* Author : Laxman
-* Date : 24-09-23
-*/
-// Dependencies
 import React, { useState } from 'react';
-import Color from './Color.jsx';
-import '../stylesheets/style.css'
+// import '../stylesheets/style.css';
 
 // Declare App component
-const App = () => {
+const Apps = () => {
   const colorName = ['red', 'green', 'blue'];
   const [color, setColor] = useState('green');
-  
-  return (
-    <div id="container" style={{backgroundColor: color}}>
-        
-           {
-              colorName.map(myColor => (
-                 <Color 
-                    onClick={() => setColor(myColor)} 
-                    key={myColor} 
-                   name={myColor}
-                   style={{backgroundColor: color}} />
-                 ))
-         }
-      
- </div>
-);
-}
 
-//Export the App component
-export default App;
+  return (
+    <div id="container" style={{ backgroundColor: color }}>
+      {colorName.map(myColor => (
+        <Color
+          onClick={() => setColor(myColor)}
+          key={myColor}
+          name={myColor}
+          style={{ backgroundColor: myColor }} // Set the background color style here
+        />
+      ))}
+    </div>
+  );
+};
+
+// Color component
+const Color = ({ name, onClick, style }) => {
+  return (
+    <>
+      <button style={style} onClick={onClick}>
+        {name}
+      </button>
+    </>
+  );
+};
+
+export default Apps;
